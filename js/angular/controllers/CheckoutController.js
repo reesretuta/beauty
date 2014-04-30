f80f097c3d2619af1a6d0e5f85cda37f446dd1ed
Brian Hankey <brian@lavisual.com>
2014-04-30 04:18:33 -0700

    added function to save a new credit card

--- js/angular/controllers/CheckoutController.js
+++ js/angular/controllers/CheckoutController.js
@@ -16,7 +16,7 @@ angular.module('app.controllers.checkout')
         }
 
         // customer data
-        $scope.customerData = {
+        $scope.existingCustomerData = {
             info: {
                     emailAddress: 'dain@lavisual.com',
                     password: 'item',
@@ -49,20 +49,46 @@ angular.module('app.controllers.checkout')
             creditCards: [
                 {
                     name: 'Dain Michael Kennison',
-                    cardNumber: 'XXXX-XXXX-XXXX-1234',
+                    cardNumber: '4321123443211234',
                     expMonth: '02',
                     expYear: '2016',
                     securityCode: '123'
                 },
                 {
                     name: 'Dain Michael Kennison',
-                    cardNumber: 'XXXX-XXXX-XXXX-1111',
+                    cardNumber: '4111111111111111',
                     expMonth: '04',
                     expYear: '2015',
                     securityCode: '123'
                 }
             ]
 
+        }
+        
+        $scope.newCustomerData = {};
+        
+        $scope.addCard = function(cardData) {
+            
+            var creditCard = {
+                    name: cardData.cardName,
+                    cardNumber: cardData.cardNumber,
+                    expMonth: cardData.expMonth,
+                    expYear: cardData.expYear,
+                    securityCode: cardData.securityCode
+            }
+            if(cardData.customerStatus=='existing') {
+                $scope.existingCustomerData.creditCards.push(angular.copy(creditCard));
+            } else {
+                var creditCards = {creditCards: []};
+                $scope.newCustomerData = creditCards;                
+                $scope.newCustomerData.creditCards.push(angular.copy(creditCard));
+            }
+                $log.debug('card data', $scope.newCustomerData); $log.debug('existing card data', $scope.existingCustomerData)
+        }
+        
+        $scope.substr = function(string, start, charNo) {
+            $scope.string = string.substr(start, charNo)
+            return $scope.result = $scope.string;
         }
 
         $scope.logStep = function() {
