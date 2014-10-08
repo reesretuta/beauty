6b424a9ea383aa27772a33a14688fd21efc977dd
David Castro <arimus@gmail.com>
2014-10-08 14:49:37 -0700

    change from red to yellow for warning

--- config/init.js
+++ config/init.js
@@ -18,9 +18,9 @@ module.exports = function() {
 	}, function(err, environmentFiles) {
 		if (!environmentFiles.length) {
 			if (process.env.NODE_ENV) {
-				console.log('\x1b[31m', 'No configuration file found for "' + process.env.NODE_ENV + '" environment using development instead');
+				console.log('\x1b[33m', 'No configuration file found for "' + process.env.NODE_ENV + '" environment using development instead');
 			} else {
-				console.log('\x1b[31m', 'NODE_ENV is not defined! Using default development environment');
+				console.log('\x1b[33m', 'NODE_ENV is not defined! Using default development environment');
 			}
 
 			process.env.NODE_ENV = 'development';
