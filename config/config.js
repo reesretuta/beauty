3ffeef3de0576dddf80d02de092be20089263c40
David Castro <arimus@gmail.com>
2014-10-07 17:40:20 -0700

    default to development

--- config/config.js
+++ config/config.js
@@ -5,8 +5,9 @@
  */
 var glob = require('glob');
 var _ = require('lodash');
+var env = process.env.NODE_ENV || "development";
 
 module.exports = _.extend(
     require('../config/env/all'),
-    require('../config/env/' + process.env.NODE_ENV) || {}
+    require('../config/env/' + env) || {}
 );
