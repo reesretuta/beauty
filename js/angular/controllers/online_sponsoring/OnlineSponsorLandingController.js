b33b6ad58164be412fe38bf10411115bd77d6022
Brian Hankey <brian@lavisual.com>
2014-10-10 10:29:10 -0700

    added language to join function commented out edit profile since users won't be able to edit their profile commented out shipping speed since we currently don't allow users to select anything other than ground hashed out href on product language dropdowns since it was going to a page not found

--- js/angular/controllers/online_sponsoring/OnlineSponsorLandingController.js
+++ js/angular/controllers/online_sponsoring/OnlineSponsorLandingController.js
@@ -4,12 +4,13 @@ angular.module('app.controllers.onlineSponsor')
 
     $rootScope.inCheckout = false;
 
-    $scope.join = function(sku) {
+    $scope.join = function(sku, language) {
         $('.modal').modal('hide');
         $('body').removeClass('modal-open');
         $('.modal-backdrop').remove();
 
         $log.debug("joining with sku", sku);
+        $log.debug("language", language);
         $location.path(JOIN_BASE_URL + "/checkout").search("sku", sku);
     };
 });
