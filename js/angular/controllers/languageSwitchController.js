ba0b4771dfd88912c9a0fcb27eb1ec48eb0a9fc8
David Castro <arimus@gmail.com>
2014-10-16 06:07:44 -0700

    language stuffs

--- js/angular/controllers/languageSwitchController.js
+++ js/angular/controllers/languageSwitchController.js
@@ -1,13 +1,19 @@
-angular.module('app.controllers.lang')
-    .controller('LanguageSwitchController', function ($scope, $document, $log, $timeout, $location, $translate, Session, $rootScope, $routeParams, JOIN_BASE_URL) {
+angular.module('app.controllers.lang').controller('LanguageSwitchController', function ($scope, $document, $log, $timeout, $location, $translate, Session, $rootScope, $routeParams, JOIN_BASE_URL) {
 
-        $scope.language = function(language) { 
-        
+        $scope.language = function (language) {
+
 //        $log.debug('current language',Session.getLanguage());
-        Session.setLanguage(language);
-        $scope.$watch('session.language', function(newVal, oldVal) {
-            $translate.use(Session.getLocalSession().language);
-        });
+            Session.setLanguage(language);
+            $scope.$watch('session.language', function (newVal, oldVal) {
+                $translate.use(Session.getLocalSession().language);
+            });
 //        $log.debug('current language',Session.getLanguage());
-    }
+        }
+
+        $scope.getCurrentLanguage = function() {
+            if (Session.getLanguage() == 'es_US') {
+                return 'LAN02';
+            }
+            return 'LAN01';
+        }
     });
