161f3d025694122aa24a241facc5a682876b34f7
Edward Hotchkiss <edward@edwardhotchkiss.com>
2014-11-23 00:27:15 -0500

    [minor, cleanup, format] ocd :)

--- js/angular/controllers/HomeController.js
+++ js/angular/controllers/HomeController.js
@@ -1,26 +1,28 @@
 
-angular.module('app.controllers.home')
-    .controller('HomeController', function ($scope, $document, $rootScope, $routeParams, $log, Categories) {
-        $rootScope.title = 'Our Products';
+angular.module('app.controllers.home').controller('HomeController', function ($scope, $document, $rootScope, $log, Categories) {
+   
+    $rootScope.title = 'Our Products';
+    $rootScope.section = "store";
 
-        $rootScope.section = "store";
+    console.log('HomeController');
 
-        console.log('HomeController');
+    $scope.objects = [];
 
-        $scope.objects = [];
-        var loadCategories = function () {
-            //var start = new Date().getTime();
-            var categories = Categories.query({}, function (value, responseHeaders) {
-                $log.debug("got categories", categories);
-                $scope.objects = categories;
-                $scope.loading = true;
-            }, function (data) {
-                //Hide loader
-                $scope.loading = false;
-                // Set Error message
-                $scope.errorMessage = "An error occurred while retrieving object list. Please refresh the page to try again, or contact your system administrator if the error persists.";
-            });
-        }
-        // kick off the first refresh
-        loadCategories();
-    });
+    var loadCategories = function () {
+        //var start = new Date().getTime();
+        var categories = Categories.query({}, function (value, responseHeaders) {
+            $log.debug("got categories", categories);
+            $scope.objects = categories;
+            $scope.loading = true;
+        }, function (data) {
+            //Hide loader
+            $scope.loading = false;
+            // Set Error message
+            $scope.errorMessage = "An error occurred while retrieving object list. Please refresh the page to try again, or contact your system administrator if the error persists.";
+        });
+    }
+
+    // kick off the first refresh
+    loadCategories();
+
+});
