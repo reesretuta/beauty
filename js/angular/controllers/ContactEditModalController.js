25cec9fee558c03a61101411639401c29751db68
Edward Hotchkiss <edward@edwardhotchkiss.com>
2014-11-23 23:44:19 -0500

    [fix, modal, validation] validate is user exists on edit info modal

--- js/angular/controllers/ContactEditModalController.js
+++ js/angular/controllers/ContactEditModalController.js
@@ -1,9 +1,9 @@
 
-angular.module('app.controllers.checkout').controller('ContactEditModalController', function ($document, HashKeyCopier, $modalInstance, $q, $scope, $rootScope, $routeParams, $location, $window, $log, $translate, Addresses, profile) {
+angular.module('app.controllers.checkout').controller('ContactEditModalController', function ($document, HashKeyCopier, $modalInstance, $q, $scope, $rootScope, $routeParams, $location, $window, $log, $translate, Addresses, profile, Session) {
 
     $log.debug('ContactEditModalController()');
 
-    $scope.profile = profile;
+    $scope.profile = angular.copy(profile);
     $scope.emailError = '';
 
     $scope.close = function () {
@@ -17,10 +17,24 @@ angular.module('app.controllers.checkout').controller('ContactEditModalControlle
     $scope.save = function () {
         $log.debug('ContactEditModalController(): save()');
         Addresses.validateEmail(profile.loginEmail).then(function (email) {
-            $log.debug('ContactEditModalController(): save(): email valid');
-            $modalInstance.close({
-                profile: profile,
-                canceled: false
+            Session.clientEmailAvailable(email, $scope.ignoreExists).then(function(available) {
+                if (available) {
+                    $log.debug('CheckoutController(): Session: client available', available);
+                    $modalInstance.close({
+                        profile: profile,
+                        canceled: false
+                    });
+                } else {
+                    $log.debug('ContactEditModalController(): save(): email invalid', error);
+                    $translate('INVALID-EMAIL').then(function (message) {
+                        $scope.emailError = message;
+                    });
+                }
+            }, function(error) {
+                $log.error('CheckoutController(): Session: client email ERROR', error);
+                $translate('INVALID-EMAIL').then(function (message) {
+                    $scope.emailError = message;
+                });
             });
         }, function(error) {
             $log.debug('ContactEditModalController(): save(): email invalid', error);
