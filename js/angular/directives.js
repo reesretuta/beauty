3503790c91ef9cecda67e9932c23e6115654b4c2
Edward Hotchkiss <edward@edwardhotchkiss.com>
2014-11-24 16:43:05 -0500

    [fix, directives] debug 1-99 directive, parseInt added, debug added, replace non-numerics added

--- js/angular/directives.js
+++ js/angular/directives.js
@@ -139,16 +139,19 @@ angular.module('app.directives', [])// directives
                     } else {
                         fixed = val;
                     }
+                    fixed = parseInt(fixed);
+                    $log.debug('Directives: oneToNinetyNine: SETTING val:', val, typeof(fixed));
                     ngModelCtrl.$setViewValue(fixed);
                     ngModelCtrl.$render();
                 }
                 // watch for changes to number
                 angular.element(elem).on('input keydown change', function (evt) {
-                    var val = parseInt(this.value);
+                    var val = this.value.replace(/[^\d\.]/g, '');
+                    $log.debug('Directives: oneToNinetyNine: GETTING val:', val, typeof(val));
                     if (val === '' || val === null) {
                         $timeout(function() {
                             runCheck(val);
-                        }, 1000);
+                        }, 250);
                     } else {
                         runCheck(val);
                     }
