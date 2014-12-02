40eba2aeb685e3ffa3b38fe8a4f2b8f1a6940e55
David Castro <arimus@gmail.com>
2014-12-02 14:31:46 -0800

    remove nodetime

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 // BASE SETUP
 // =============================================================================
 
-if (process.env.NODETIME_ACCOUNT_KEY) {
-    require('nodetime').profile({
-        accountKey: process.env.NODETIME_ACCOUNT_KEY,
-        appName: 'My Application Name' // optional
-    });
-}
-
 // call the packages we need
 require('newrelic');
 var init = require('./config/init')();
