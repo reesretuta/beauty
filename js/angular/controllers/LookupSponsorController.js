2c0eaea385cacb16b2f8b4d17c5070955f44649f
Edward Hotchkiss <edward@edwardhotchkiss.com>
2015-05-01 11:59:26 -0700

    [cleanup, sponsors, search] cleanup controllers and partials and setup for actual wiring of search for sponsor by name

--- js/angular/controllers/LookupSponsorController.js
+++ js/angular/controllers/LookupSponsorController.js
@@ -1,91 +1,30 @@
 
-angular.module('app.controllers.checkout').controller('LookupSponsorController', function ($document, HashKeyCopier, $modalInstance, $q, $scope, $rootScope, $routeParams, $location, $window, $log, $translate, Addresses, Session, Consultant) {
+angular.module('app.controllers.checkout').controller('LookupSponsorController', function ($document, $modalInstance, $q, $scope, $log, $translate) {
 
-    $log.debug('LookupSponsorController()');
-    
-    // $scope.profile = {
-    //     zip: '',
-    //     firstName: '',
-    //     lastName: ''
-    // }
-    
-    
-    $scope.search = function(){
-        // if ($scope.profile.firstName != '' && $scope.profile.lastName != '') {
-        //     Consultant.searchByName();
-        //
-        // }else{
-        //     Consultant.searchByZip(12345).then(function(data){
-        //
-        //     });
-        // }
-        
+    $log.debug('LookupSponsorController() loaded.');
+
+    $scope.search = function () {
+        $log.debug('LookupSponsorController(): search()');
         $scope.sponsors = [
-            {name: 'test test', id: '123', city: 'city'},
-            {name: 'test test', id: '123', city: 'city'},
-            {name: 'test test', id: '123', city: 'city'},
-            {name: 'test test', id: '123', city: 'city'},
-            {name: 'test test', id: '123', city: 'city'}
+            { name: 'test test', id: '123', city: 'city'},
+            { name: 'test test', id: '123', city: 'city'}
         ];
-        
-    }
+    };
 
     $scope.close = function () {
-        $log.debug('LookupSponsorController()');
+        $log.debug('LookupSponsorController(): close()');
         $modalInstance.close({
-            canceled : true
+            sponsorId : null,
+            canceled  : true
         });
     };
 
-    $scope.selectSponsor = function(sponsorId){
+    $scope.selectSponsor = function (sponsorId) {
+        $log.debug('LookupSponsorController(): selectSponsor(): sponsorId:', sponsorId);
         return $modalInstance.close({
-            sponsorId  : sponsorId,
-            canceled : false
+            sponsorId : sponsorId,
+            canceled  : false
         });
-    };
-    
-    $scope.save = function () {
-        // $log.debug('LookupSponsorController(): save(): trying email:', $scope.profile.loginEmail);
-        // if (JSON.stringify($scope.profile) === JSON.stringify(originalProfile)) {
-        //     return $modalInstance.close({
-        //         profile  : $scope.profile,
-        //         canceled : false
-        //     });
-        // }
-        // if (JSON.stringify($scope.profile) === JSON.stringify(originalProfile)) {
-        //     return $modalInstance.close({
-        //         profile  : $scope.profile,
-        //         canceled : false
-        //     });
-        // }
-        // Addresses.validateEmail($scope.profile.loginEmail).then(function (email) {
-//             Session.consultantEmailAvailable($scope.profile.loginEmail, false).then(function(available) {
-//                 if (available) {
-//                     $log.debug('CheckoutController(): Session: client available', available);
-//                     $modalInstance.close({
-//                         profile  : $scope.profile,
-//                         canceled : false
-//                     });
-//                 } else {
-//                     $log.debug('ContactEditModalController(): save(): email invalid');
-//                     $translate('INVALID-EMAIL-ADDRESS-IN-USE').then(function (message) {
-//                         $log.debug('ContactEditModalController(): INVALID-EMAIL-ADDRESS-IN-USE');
-//                         $scope.emailError = message;
-//                     });
-//                 }
-//             }, function(error) {
-//                 $log.error('CheckoutController(): Session: client email ERROR', error);
-//                 $translate('INVALID-EMAIL-ADDRESS-IN-USE').then(function (message) {
-//                     $log.debug('ContactEditModalController(): INVALID-EMAIL-ADDRESS-IN-USE');
-//                     $scope.emailError = message;
-//                 });
-//             });
-//         }, function(error) {
-//             $log.debug('ContactEditModalController(): save(): email invalid', error);
-//             $translate('INVALID-EMAIL').then(function (message) {
-//                 $scope.emailError = message;
-//             });
-//         });
     };
 
     $scope.$on('$destroy', function() {
