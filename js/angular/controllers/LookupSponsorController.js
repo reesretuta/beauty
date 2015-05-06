ed7c478c442a1b0a3c40d4073ecec2ce23059b0b
Edward Hotchkiss <edward@edwardhotchkiss.com>
2015-05-06 15:47:16 -0700

    [major, sponsors, search] spanish translations for all fields, form clearing on change, cleaner logic

--- js/angular/controllers/LookupSponsorController.js
+++ js/angular/controllers/LookupSponsorController.js
@@ -3,28 +3,77 @@ angular.module('app.controllers.checkout').controller('LookupSponsorController',
 
     $log.debug('LookupSponsorController() loaded.');
 
+    // forms object for having access to form methods - initiate collection
+    $scope.forms = {};
+
+    // success message w/ results?
+    $scope.successMessage = null;
+
+    // error message / no results?
+    $scope.errorMessage = null;
+
     function search (type) {
         var payload;
-        // clear current results, if any
+        // clear current results, if any and messages
         $scope.sponsors = [];
+        $scope.successMessage = null;
+        $scope.errorMessage = null;
         $log.debug('LookupSponsorController(): search(): type:', type);
         if (type === 'zip') {
+            // clear search by name
+            $scope.firstName = '';
+            $scope.forms.sponsorByNameForm.firstName.$setPristine();
+            $scope.forms.sponsorByNameForm.firstName.$setUntouched();
+            $scope.lastName = '';
+            $scope.forms.sponsorByNameForm.lastName.$setPristine();
+            $scope.forms.sponsorByNameForm.lastName.$setUntouched();
+            // setup post data
             payload = {
                 zip : $scope.zip
-            }
+            };
         } else if (type === 'name') {
+            // clear search by zip if any
+            $scope.zip = '';
+            $scope.forms.sponsorByZipForm.zip.$setPristine();
+            $scope.forms.sponsorByZipForm.zip.$setUntouched();
+            // setup post data
             payload = {
                 firstName : $scope.firstName,
                 lastName  : $scope.lastName
-            }
+            };
         }
         // perform actual search after payload setup
         $log.debug('LookupSponsorController(): search(): payload:', payload);
         Sponsor.search(payload).then(function (sponsors) {
             $scope.sponsors = sponsors;
             $log.debug('LookupSponsorController(): sponsors:', sponsors);
+            if ($scope.sponsors.length && $scope.sponsors.length > 0) {
+                if (type === 'zip') {
+                   $translate('SPONSORS-SEARCH-FOUND-ZIP', { 
+                        zip   : $scope.zip,
+                        count : $scope.sponsors.length
+                    }).then(function (message) {
+                        $scope.successMessage = message;
+                    });
+                } else if (type === 'name') {
+                    $translate('SPONSORS-SEARCH-FOUND-NAME', {
+                        firstName : $scope.firstName,
+                        lastName : $scope.lastName,
+                        count    : $scope.sponsors.length
+                    }).then(function (message) {
+                        $scope.successMessage = message;
+                    });
+                }
+            } else {
+                $translate('SPONSORS-SEARCH-NO-SPONSORS-FOUND').then(function (message) {
+                    $scope.errorMessage = message;
+                });
+            }
         }, function (error) {
             $log.error('LookupSponsorController(): search(): error:', error);
+            $translate('SPONSORS-SEARCH-UNKNOWN-ERROR').then(function (message) {
+                $scope.errorMessage = message;
+            });
         });
     };
 
