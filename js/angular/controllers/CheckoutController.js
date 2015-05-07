698ac6a16e7df37ab59b6f13f17e0fe969558d4f
Michael Kwon <mkkwon11@gmail.com>
2015-05-07 16:44:37 -0700

    [JAFOS-57] Making checkout controller method to detect if it is in the cart.

--- js/angular/controllers/CheckoutController.js
+++ js/angular/controllers/CheckoutController.js
@@ -611,6 +611,15 @@ angular.module('app.controllers.checkout')
             return d.promise;
         }
         
+        $scope.isInCart = function(sku){
+          var isInCart = false;
+          for (var i = 0; i < $scope.cart.length; i++) {
+              if ($scope.cart[i].sku == sku) {
+                  isInCart = true;
+              }
+          }
+          return isInCart;
+        }
         
         $scope.selectQncProduct = function(sku) {
             $log.debug("CheckoutController(): selectQncProduct(): $scope.cart", $scope.cart);
